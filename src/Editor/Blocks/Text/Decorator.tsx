d0508429df66d221f9bf6ad96899ead88ea7d031
nuemu <tusko.ku@gmail.com>
2022-07-15 14:53:15 +0900

    Fix: eq & url

--- src/Editor/Blocks/Text/Decorator.tsx
+++ src/Editor/Blocks/Text/Decorator.tsx
@@ -1,5 +1,8 @@
-import { Component, createEffect, createSignal } from "solid-js"
+import { Component, createEffect, createSignal, Show } from "solid-js"
 import Separator from "./Separator"
+
+import katex from 'katex'
+import 'katex/dist/katex.css'
 
 const styles: text_styles = {
   emphasis: {
@@ -9,6 +12,12 @@ const styles: text_styles = {
   strikethrough: {
     'text-decoration': 'line-through'
   },
+  invisible: {
+    'position': 'absolute',
+    'height': '0px',
+    'width': '0px',
+    'opacity': 0
+  }
 }
 
 const Decorator: Component<TextBlockProps> = (props: TextBlockProps) => {
@@ -16,7 +25,34 @@ const Decorator: Component<TextBlockProps> = (props: TextBlockProps) => {
     return props.focusing && props.node.start! <= props.caret && props.node.end! >= props.caret
   }
 
-  return <span style={styles[props.node.type]}><Separator node={props.node} caret={props.caret} focusing={props.focusing} caret_on={caret_inside()} /></span>
+  const switch_style = () => {
+    return !caret_inside() && (props.node.type === 'equation' || props.node.type === 'url') ?  'invisible' : props.node.type
+  }
+
+  return (
+    <>
+      <Show when={!caret_inside() && props.node.type==='equation'}>
+        <span
+          class="katex-base"
+          contentEditable={false}
+          innerHTML={katex.renderToString(props.node.content, {
+            throwOnError: false
+          })}
+        />
+      </Show>
+      <Show when={!caret_inside() && props.node.type==='url'}>
+        <a
+          contentEditable={false}
+          href="/Editor"
+        >
+          <Separator node={props.node} caret={props.caret} focusing={props.focusing} caret_on={caret_inside()} />
+        </a>
+      </Show>
+      <span style={styles[switch_style()]}>
+        <Separator node={props.node} caret={props.caret} focusing={props.focusing} caret_on={caret_inside()} />
+      </span>
+    </>
+  )
 }
 
 export default Decorator
