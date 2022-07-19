0b41288d9b936f38a25e192c23001a6c008f728b
nuemu <tusko.ku@gmail.com>
2022-07-19 20:35:29 +0900

    Add: ESLint

--- src/Editor/Utils/Paragraph.ts
+++
@@ -1,9 +0,0 @@
-import { Accessor, createSignal, Setter } from "solid-js";
-
-export default class Paragraph {
-  paragraph: Accessor<Paragraph>
-  setParagraph: Setter<Paragraph>
-  constructor(paragraph: Paragraph){
-    [this.paragraph, this.setParagraph] = createSignal(paragraph)
-  }
-}
